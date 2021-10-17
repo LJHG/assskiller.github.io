0d3bfff7a8e22cc8109774229153c3676b64058e
assskiller <674478778@qq.com>
2021-10-17 19:49:14 +0800

    update from gitbook

---
+++ publish.sh
@@ -0,0 +1,19 @@
+gitbook build . ../tmp_mysite
+
+function publish_to()
+{
+    repo_name=$1
+    cd ../${repo_name}
+    rm -rf `ls |grep -v .git`
+    cp -r ../tmp_mysite/* ../${repo_name}
+    cd ../${repo_name}
+    git add -A
+    git commit -a -m 'update from gitbook'
+    git push
+}
+
+publish_to mywebsite
+# publish_to LJHG.github.io
+
+rm -rf ../tmp_mysite
+# ssh cwh@cweihang.io "cd mysite;git pull"
